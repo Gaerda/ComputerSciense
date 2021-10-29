4b9a19029a1bf50b13570fa1ee89d9c644e74b6e
Dauletbekov Erbolat <90600901+Gaerda@users.noreply.github.com>
2021-10-29 13:24:30 +0600

    Add files via upload

---
+++ web2/main.js
@@ -0,0 +1,29 @@
+document.getElementById('main-form').addEventListener("sumbit", checkForm);
+
+function checkForm(event) {
+    event.preventDefault();
+    var el = document.getElementById('main-form');
+
+    var name = el.name.value;
+    var pass = el.pass.value;
+    var repass = el.repass.value;
+    var state = el.state.value;
+
+    var fail = "";
+
+    if(name == "" || pass == || state == "")
+        fail = "Заполните все поле";
+    else if(name.length <=1 || name.length > 50)
+        fail = "Введите корректное имя";
+    else if(pass != repass)
+        fail = "Пароли должны совпадать";
+    else if(pass.split("&").length > 1)
+        fail = "Некорректный пароль";
+
+    if(fail !="")
+        document.getElementById('error').innerHTML = fail;
+    else {
+        alert("Все данные корректно заполнены");
+        window.location = 'https://itproger.com';
+    }
+}
